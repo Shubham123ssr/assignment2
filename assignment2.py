70da006277ed0a2e81bf0c0025a652b96b482e31
Shubham123ssr <100044183+Shubham123ssr@users.noreply.github.com>
2022-09-01 06:32:48 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,43 @@
+
+#print("Learning Python")
+'''
+name="shubham"
+age=19
+a=3
+b=5
+print (name,age,a,b)
+'''
+
+'''
+
+
+a=35
+value="true"
+name="MysirG"
+number=5.46
+complexi=3+4j
+print(a,value,name,number,complexi)
+'''
+'''help()'''
+'''import keyword
+print("all keywords in python are")
+print(keyword.kwlist)
+'''
+'''import a1
+print(a1.x)
+d=1
+m=9
+y=2022
+h=6
+mi=25
+s=12
+a="pm"
+print(d,"-",m,"-",y)
+print(h,":",mi,":",s," ",a)
+'''
+
+
+
+
+
+
